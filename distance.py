2786fdf0e04d682ca64bd6a5e40ce1e4551d3653
Garren Ramirez <gramirez4466@sdsu.edu>
2025-03-08 18:30:58 -0800

    Final Commit: Assignment Completed

--- distance.py
+++ distance.py
@@ -4,10 +4,10 @@ class Distance:
     """
     Class to represent a distance measurement with timestamp.
     """
-    def __init__(self, distance: float, timestamp: str = datetime.now().strftime("%M: %S")):
+    def __init__(self, distance: float):
         """Initialize the Distance object with distance and timestamp."""
         self.distance = distance
-        self.timestamp = timestamp
+        self.timestamp = datetime.now().strftime("%H:%M:%S")
 
     def __str__(self):
         """Return a formatted string representation of the Distance object."""
